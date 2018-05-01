f0703557c1bb60f1a6123a238cff440143a9db66
Nico teWinkel <ntewinkel@gmail.com>
2018-04-30 17:32:59 -0700

    Updated to allow graphing of specified sensor

--- ESP-01/getData.php
+++ ESP-01/getData.php
@@ -2,28 +2,31 @@
 
 // It reads a json formatted text file and outputs it.
 
-    echo' {
-        "cols": [
-        {"label":"Time","type":"string"},
-        {"label":"Temperature C","type":"number"}
-        ],
-        "rows": [
-    ';
-    
-    $filename = "log_001.csv";
-    $handle = fopen($filename, "r");
-    if ($handle) {
-        while (($line = fgets($handle)) !== false) {
-            // process the line read.
-            $columns = explode(",", $line);
-            echo '{"c":[{"v":"' . $columns[0] . '"},{"v":' . $columns[1] . '}]},';
-        }
-        
-        fclose($handle);
-    } else {
-        // error opening the file.
-        echo "<tr><td align=\"center\">error reading log file.</td>";
+$sensorID = $_GET["sensorID"];
+
+echo' {
+    "cols": [
+    {"label":"Time","type":"string"},
+    {"label":"Sensor ' . $sensorID . '","type":"number"}
+    ],
+    "rows": [
+';
+
+$filename = "log_" . $sensorID . ".csv";
+$handle = fopen($filename, "r");
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        // process the line read.
+        $columns = explode(",", $line);
+        echo '{"c":[{"v":"' . $columns[0] . '"},{"v":' . $columns[1] . '}]},';
     }
     
-    echo ']}';
+    fclose($handle);
+} else {
+    // error opening the file.
+    echo "<tr><td align=\"center\">error reading log file.</td>";
+}
+
+echo ']}';
+
 ?>
